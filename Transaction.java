8ea79ddc81f3bfd5b60c43073f7f7ff1b5b2ced2
Shagun Singh <shagundun@gmail.com>
2025-09-06 16:53:23 +0530

    feat: Transaction class added and inputs for transaction details enabled

---
+++ Transaction.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Transaction {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Let's add a transaction you made!");
+        System.out.println("Enter the amount you withdrew: ");
+        int amtWithdrawn = sc.nextInt();
+        System.out.println("Enter the reason for which you withdrew this amount: ");
+        String type = sc.nextLine();
+        System.out.println("Enter the date you withdrew: ");
+        int date = sc.nextInt();
+        System.out.println("Enter the category of transaction: ");
+        int category = sc.nextInt();
+    }
+    
+}
