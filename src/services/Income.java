df9396ae89bc780c6d66148a442ff75fc65c0fc5
Shagun Singh <shagundun@gmail.com>
2025-09-09 16:41:43 +0530

    feat: Added ability to record income

---
+++ src/services/Income.java
@@ -0,0 +1,18 @@
+package services;
+import java.util.*;
+
+public class Income {
+    public static void main(String args[])
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Let's record your income!");
+    System.out.println("How much do you earn per month?");
+    int incomePerMonth = sc.nextInt();
+    System.out.println("What's the source of this income?");
+    String incomeSource = sc.nextLine();
+    }
+
+
+
+    
+}
