4dcb3b410dcaa7e771e15cc8cf54c5d9bf90ad5b
Shagun Singh <shagundun@gmail.com>
2025-09-10 15:58:59 +0530

    feat: Added ability to track transactions

---
+++ src/utility/TransactionHandler.java
@@ -0,0 +1,21 @@
+package utility;
+
+public class TransactionHandler {
+
+    private int amt;
+    private String ty;
+    private int dat;
+    private String cat;
+
+    public TransactionHandler(int amtWithdrawn, String type, int date, String category)
+    {
+        amt = amtWithdrawn;
+        ty = type;
+        dat = date;
+        cat = category;
+    }
+    public static void main (String args[])
+    {
+        
+    }
+}
