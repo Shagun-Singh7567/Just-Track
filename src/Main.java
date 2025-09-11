010afff52b0c1d21aa7541959ffa1d0d1b809793
Shagun Singh <shagundun@gmail.com>
2025-09-11 18:34:09 +0530

    feat: Added basic CLI to Main class for managing main menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+import services.*;
 public class Main {
-    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to JustTrack - A simple budget tracker, made to help you be more financially responsible");
+        System.out.println("Press the specific letters in the terminal to perform the interaction you wish to perform: ");
+        System.out.println("To make a transaction, press T");
+        System.out.println("To record your income, press I");
+        System.out.println("To record an expense, press E");
+        char choiceInput= sc.next().charAt(0);
+        switch(choiceInput)
+        {
+            case 'T':
+                Transaction newTransaction = new Transaction();
+
+            case 'I':
+                Income newIncomeRecord = new Income();
+
+            case 'E':
+                Expense newExpenseRecord = new Expense();
+        }
+
+        
+    }
 }
