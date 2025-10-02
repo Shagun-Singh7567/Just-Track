f3b1c1a6d51dd74471a1f8c7fdae5866bdb0d04a
Shagun Singh <shagundun@gmail.com>
2025-10-02 07:33:04 +0530

    fix: Fixed date format for better readability

--- src/utility/DateManager.java
+++ src/utility/DateManager.java
@@ -5,7 +5,9 @@ public class DateManager {
     public String currentDateGetter()
     {
         Date d1 = new Date();
-        return d1.toString();
+        String date = d1.toString();
+        date = date.substring(0,11) + date.substring(24,28);
+        return date;
     }
 
     public void datePrinter()
