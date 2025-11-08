3549c958e8fd88f56a09f7d1e15c2c79101230f1
Shagun Singh <shagundun@gmail.com>
2025-11-08 19:41:23 +0530

    feat: Added a new class AuthHandler which contains methods for handling user login, sign up and hashing

---
+++ src/AuthManager.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AuthManager {
+    Scanner sc = new Scanner(System.in);
+    public void signUp()
+    {
+       
+    }
+
+    public void login()
+    {
+        
+    }
+
+    String hash(String pass)
+    {
+        return "";
+    }
+}
