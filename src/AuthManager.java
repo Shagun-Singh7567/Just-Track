af7c2b1d86d4abf9cabe4d92eefa3ae011ea579e
Shagun Singh <shagundun@gmail.com>
2025-11-10 19:43:25 +0530

    feat: Added signup logic to AuthHandler class and added method for username uniqueness check

--- src/AuthManager.java
+++ src/AuthManager.java
@@ -4,16 +4,52 @@ public class AuthManager {
     Scanner sc = new Scanner(System.in);
     public void signUp()
     {
+       System.out.println("Let's create your account!");
+       String username = "";
+       String password = "";
+       while(usernameValidator(username))
+       {
+       System.out.println("Enter a username: ");
+       username = sc.nextLine();
+       }
+       do
+       {
+       System.out.println("Enter your password: ");
+       password = sc.nextLine();
+       }
+       while(passwordValidator(password));
+       password = hash(password);
        
     }
 
     public void login()
     {
-        
+
     }
 
-    String hash(String pass)
+    private String hash(String pass)
     {
         return "";
     }
+
+    boolean usernameValidator(String uname)
+    {
+        if(uname != "already_taken_username")
+        {
+        System.out.println("Hey, that's a cool name, " + uname);
+        return true;
+        }
+
+        else
+        {
+        System.out.println("This username is already taken, please try again!");
+        return false;
+        }
+    }
+
+    boolean passwordValidator(String pass)
+    {
+        return false;
+    }
+
 }
