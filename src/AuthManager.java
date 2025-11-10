46a758556a30f0ce53ec8d55a1e8a00842434a8d
Shagun Singh <shagundun@gmail.com>
2025-11-10 20:26:56 +0530

    feat: Added password validation

--- src/AuthManager.java
+++ src/AuthManager.java
@@ -49,7 +49,51 @@ public class AuthManager {
 
     boolean passwordValidator(String pass)
     {
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean digit = false;
+        boolean specialCharacter = false;
+        if(pass.length() < 8 || pass.length() > 15) // Password length
+        {
+        System.out.println("Please maintain password length between 8 and 15");
         return false;
+        }
+        
+        for(int i = 0; i < pass.length(); i++)
+        {
+        char ch = pass.charAt(i);
+        
+        if(Character.isWhitespace(ch))
+            {
+            System.out.println("Passwords can't contain empty spaces!");
+            return false;
+            }
+        
+        if(Character.isUpperCase(ch))
+        upperCase = true;
+
+        if(Character.isLowerCase(ch))
+        lowerCase = true;
+
+        if(Character.isDigit(ch))
+        digit = true;
+
+        if(Character.isLetterOrDigit(ch) == false)
+        specialCharacter = true;
+
+        }
+
+        if(upperCase == false || lowerCase == false || digit == false || specialCharacter == false)
+        {
+            System.out.println("You are missing either/all of the following: an upper case letter, a lower case letter, a digit, or a special character. Please try again");
+        }
+
+        return true;
     }
 
+    public static void main(String args[])
+    {
+        AuthManager obj = new AuthManager();
+        obj.signUp();
+    }
 }
